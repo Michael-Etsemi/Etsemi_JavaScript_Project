b8e669cde1b3acffc6d0ba6c634c52d2b5806866
Michael-Etsemi <ejutemieden.michael@gmail.com>
2023-05-25 13:13:13 +0100

    My first commit.

--- shellcode.sh
+++ shellcode.sh
@@ -2,3 +2,4 @@
 echo I am a good programmer
 echo This programming is good thing to actually learn.
 echo I am getting used to the programming language shortly.
+echo I am getting better at mastering the formular.
