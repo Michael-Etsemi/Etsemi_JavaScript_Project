508ceb0abcb649fdc809c086489f2ce97afca74a
Etsemi Michael <134491757+Michael-Etsemi@users.noreply.github.com>
2023-05-29 16:15:20 +0100

    Update shellcode1.sh

--- shellcode1.sh
+++ shellcode1.sh
@@ -3,4 +3,4 @@
 echo The man is a good man.
 echo I am going to revise the conditional statements in shell script.
 echo "This is the main idea of fixing merg conflicts"
-echo "Do this and kindly inform me."
+echo "Do this and kindly inform me"
